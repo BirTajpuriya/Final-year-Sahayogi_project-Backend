f87c584e4eec3542bdb9a389ab0ac4f32f81f9bb
BirTajpuriya <tajpuriyabir00@gmail.com>
2023-10-09 11:35:14 +0545

    updated to 22

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const url = process.env.MONGO_URI;
 export const connectDB = async () => {
   try {
-    let connection = await mongoose.connect(
+    let { connection } = await mongoose.connect(
       "mongodb://127.0.0.1:27017/sahayogi"
     );
     console.log(`connected to local database`);
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,7 +1,7 @@
 import { Course } from "../models/Course.js";
 
 export const getAllCourses = async (req, res, next) => {
-  res.send("working");
+  //res.send("working");
 
   const courses = await Course.find();
   res.status(200).json({
